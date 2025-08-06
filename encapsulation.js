f409f14512c2bb58ceb180f3dcac38424e131714
Aqeela Fazle Mawla Ramadhan <affirmexe@gmail.com>
2025-08-06 09:55:06 +0700

    Update encapsulation.js

--- encapsulation.js
+++ encapsulation.js
@@ -1,4 +1,4 @@
-// // 1. Konvensi (PSEUDO-ENCAPSULATION)
+// 1. Konvensi (PSEUDO-ENCAPSULATION)
 class BankAccount {
   constructor() {
     this._balance = 0;
@@ -36,3 +36,4 @@ const akun = new SecureAccount();
 akun.deposit(500);
 // akun.#balance = 9999; Attempt to modify the private property, this will throw an error
 akun.showBalance(); // Output: Current balance aman is: 500 (still the original value, cannot modify directly)
+
